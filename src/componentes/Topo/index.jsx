121ba2fcd83f9b1c2351b2d11a626006cbc18e18
Flávio Félix <fau_felix30@hotmail.com>
2025-09-01 16:41:08 -0300

    Done componente Topo e algumas atualizações

--- src/componentes/Topo/index.jsx
+++ src/componentes/Topo/index.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Logo from "/public/vercel.svg";
-import styles from "./Topo.module.css";
+import estilos from "./Topo.module.css";
 
 export default function Topo() {
   return (
-    <div className={styles.topo_container}>
-      <Image className={styles.img_logo} src={Logo} alt="Vercel Logo" />
-      <h1>Topo</h1>
+    <div className={estilos.topo_container}>
+      <Image className={estilos.img_logo} src={Logo} alt="Vercel Logo" />
+      <h1 className={estilos.titulo}>Topo</h1>
     </div>
   );
 }
